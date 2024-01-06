1b70d666660e786690b1c6c4a2c9641f0bebd03b
Abdelhalim ben oun <abdelhalimbenoun@gmail.com>
2024-01-06 16:36:56 +0100

    add api key

--- src/zustand/zustand.ts
+++ src/zustand/zustand.ts
@@ -4,4 +4,7 @@ import {AuthStoreType} from './types/auth-type';
 export const useAuthStore = create<AuthStoreType>(set => ({
   isAuthed: false,
   updateAuthStore: () => set(state => ({isAuthed: !state.isAuthed})),
+  //   apiCall:async()=> {const response=await new Promise((resolve)=>APICall)
+  //set(state)=>({response:state.response})
+  //}
 }));
