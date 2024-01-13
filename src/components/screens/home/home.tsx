2be63682bb443bb1dc4e93fc0d10e414c89bef08
Abdelhalim ben oun <abdelhalimbenoun@gmail.com>
2024-01-13 14:52:01 +0100

    axios tests not set yet

--- src/components/screens/home/home.tsx
+++ src/components/screens/home/home.tsx
@@ -1,12 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import useFetchCats from '../../../custom-hooks/fetch-cats';
+// import useFetchCats from '../../../custom-hooks/fetch-cats';
 
 const Home = () => {
-  const {data, isLoading, isError} = useFetchCats();
+  // const {data, isLoading, isError} = useFetchCats();
   return (
-    <View style={{flex: 1}}>
-      <Text style={{color: '#000'}}>home</Text>
+    <View style={{flex: 1}} accessibilityRole="text">
+      <Text style={{color: '#000'}} accessibilityRole="text">
+        home
+      </Text>
     </View>
   );
 };
