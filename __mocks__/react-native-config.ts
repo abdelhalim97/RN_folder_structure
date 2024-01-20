6f4616da5dda3cfeb467925fdf6f5e649d854a14
Abdelhalim ben oun <abdelhalimbenoun@gmail.com>
2024-01-20 13:32:51 +0100

    mock react native env library

---
+++ __mocks__/react-native-config.ts
@@ -0,0 +1,9 @@
+//i know these keys shouldnt be pushed to github but if i hide them and someone clone the repo the project wont work
+//i think providing mocking example
+export default {
+  ENVIRONMENT: 'dev',
+  CAT_API_KEY:
+    'live_Mi0gvX7xgYU4kAW5dlOyRknsNmVqqXl47Fvt8A5UGMkHmG5RP8MT2N9TjAvxpv8D',
+  CAT_API_URL: 'https://api.thecatapi.com/v1/',
+  SERVER: '127.0.0.1:3000',
+};
